0628f1fff5460f51a5a6d880a7b5a5e1ccf6e085
Ömer Öncü <omer.oncu@yandex.com>
2025-09-30 21:41:25 +0300

    İlk React Projem

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Ovrika</h1>
+      <p>Web sitemiz yapım aşamasında 🚧</p>
+    </div>
+  );
+}
+
+export default App;
